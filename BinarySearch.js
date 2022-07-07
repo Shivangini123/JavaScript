81226e6750ded11fec0dc15c44b4adea480400db
Shivangini123 <103523491+Shivangini123@users.noreply.github.com>
2022-07-07 20:22:39 +0530

    Create BinarySearch.js

---
+++ BinarySearch.js
@@ -0,0 +1,26 @@
+<script>
+function binarySearch(arr, l, r, x){
+  if (r >= l) {
+    let mid = l + Math.floor((r - l) / 2);
+
+   if (arr[mid] == x)
+      return mid;
+
+     if (arr[mid] > x)
+      return binarySearch(arr, l, mid - 1, x);
+
+      return binarySearch(arr, mid + 1, r, x);
+  }
+
+   return -1;
+}
+
+let arr = [ 4, 6, 5, 20, 40 ];
+let x = 20;
+let n = arr.length
+let result = binarySearch(arr, 0, n - 1, x);
+(result == -1) 
+        ? document.write( "Element is not present in array")
+        : document.write("Element is present at index " +result);
+</script>
+
